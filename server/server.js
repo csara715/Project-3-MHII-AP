8f65f9f1d4188f6ae51535d14605d6fe5403fe90
csara <csara715@gmail.com>
2023-02-26 09:41:48 -0700

    trying to fix heroku reload

--- server/server.js
+++ server/server.js
@@ -17,8 +17,15 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static("client/build"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
 
 app.get("/", (req, res) => {
